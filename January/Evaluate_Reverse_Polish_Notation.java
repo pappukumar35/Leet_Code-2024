c1d5e1c83fbffc3bb32285255d6770ac13aacbe3
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2024-01-30 21:18:45 +0530

    Add files via upload

---
+++ January/Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+class Evaluate_Reverse_Polish_Notation {
+    long resolves(long a, long b, char Operator) {
+        if (Operator == '+')
+            return a + b;
+        else if (Operator == '-')
+            return a - b;
+        else if (Operator == '*')
+            return a * b;
+        return a / b;
+    }
+
+    public int evalRPN(String[] tokens) {
+        Stack<Long> stack = new Stack<>();
+        int n = tokens.length;
+        for (int i = 0; i < n; i++) {
+            if (tokens[i].length() == 1 && tokens[i].charAt(0) < 48) {
+                long integer2 = stack.pop();
+                long integer1 = stack.pop();
+                char operator = tokens[i].charAt(0);
+                long resolvedAns = resolves(integer1, integer2, operator);
+                stack.push(resolvedAns);
+            } else {
+                stack.push(Long.parseLong(tokens[i]));
+            }
+        }
+        return stack.pop().intValue();
+    }
+}
