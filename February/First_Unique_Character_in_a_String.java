2a42ac217b0d2d6f9563b2cc7002c00c9aac70af
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2024-02-05 21:14:31 +0530

    Add files via upload

---
+++ February/First_Unique_Character_in_a_String.java
@@ -0,0 +1,17 @@
+class First_Unique_Character_in_a_String {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+
+        for (char a : s.toCharArray()) {
+            mp.put(a, mp.getOrDefault(a, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (mp.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
