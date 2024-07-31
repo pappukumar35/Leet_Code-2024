b9e989af4297d65117a9915a445aba2ce7c8c873
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2024-07-31 21:05:29 +0530

    Add files via upload

---
+++ July/Filling_Bookcase_Shelves.java
@@ -0,0 +1,27 @@
+package July;
+
+import java.util.*;
+
+public class Filling_Bookcase_Shelves {
+     public int minHeightShelves(int[][] books, int shelfWidth) {
+          int n = books.length;
+          int[] f = new int[n + 1];
+          for (int i = 1; i <= n; ++i) {
+               int w = books[i - 1][0], h = books[i - 1][1];
+               f[i] = f[i - 1] + h;
+               for (int j = i - 1; j > 0; --j) {
+                    w += books[j - 1][0];
+                    if (w > shelfWidth) {
+                         break;
+                    }
+                    h = Math.max(h, books[j - 1][1]);
+                    f[i] = Math.min(f[i], f[j - 1] + h);
+               }
+          }
+          return f[n];
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
