62f21fddd44dcec3647489fd1c769fc3f9be9c6e
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2024-11-30 12:28:04 +0530

    Add files via upload

---
+++ November/Valid_Arrangement_of_Pairs.java
@@ -0,0 +1,58 @@
+package November;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Valid_Arrangement_of_Pairs {
+     List<Integer> circuit;
+
+     public int[][] validArrangement(int[][] pairs) {
+          Map<Integer, List<Integer>> graph = new HashMap();
+          Map<Integer, Integer> node = new HashMap();
+
+          for (int[] pair : pairs) {
+               if (!graph.containsKey(pair[0])) {
+                    graph.put(pair[0], new ArrayList());
+               }
+               graph.get(pair[0]).add(pair[1]); // Preparing graph
+               node.put(pair[0], node.getOrDefault(pair[0], 0) - 1); // Outgoing
+               node.put(pair[1], node.getOrDefault(pair[1], 0) + 1); // Incoming
+          }
+
+          // selecting the starting node
+          int startNode = pairs[0][0];
+          for (Map.Entry<Integer, Integer> enty : node.entrySet()) {
+               if (enty.getValue() == -1) {
+                    startNode = enty.getKey();
+                    break;
+               }
+          }
+
+          circuit = new ArrayList();
+          dfs(graph, startNode);
+          Collections.reverse(circuit);
+
+          int[][] result = new int[pairs.length][2];
+          for (int i = 1; i < circuit.size(); i++) {
+               result[i - 1][0] = circuit.get(i - 1);
+               result[i - 1][1] = circuit.get(i);
+          }
+
+          return result;
+     }
+
+     void dfs(Map<Integer, List<Integer>> graph, int u) {
+          while (graph.containsKey(u) && !graph.get(u).isEmpty()) {
+               int v = graph.get(u).remove(0);
+               dfs(graph, v);
+          }
+          circuit.add(u);
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
